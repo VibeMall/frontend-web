65ba994489fb6d77a2534ee8ca5dc69024cca301
Phuc Nguyen Hoang <116567243+slo248@users.noreply.github.com>
2025-09-14 11:40:30 +0700

    4: Publish Github Pages (#5)
    * 4: Recognize Vibe Mall
    
    * 3: Add workflow

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Footer = () => {
   return (
@@ -6,12 +5,16 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
+            <p className="mb-3 mb-md-0">
+              Inspired by{" "}
+              <a href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">
+                Sahib Singh
+              </a>
+              {" "}
+              <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
+                <i className="fa fa-github"></i>
+              </a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
           </div>
         </div>
       </footer>
